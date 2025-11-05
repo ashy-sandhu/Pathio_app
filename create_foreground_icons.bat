dd41fc63b559ba0520d642b60b873f804b4628a7
Ashy-Sandhu <ahsan.build@gmail.com>
2025-11-05 11:27:05 +0500

    10

--- create_foreground_icons.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-REM Copy ic_launcher.png to ic_launcher_foreground.png in all mipmap folders
-copy "android\app\src\main\res\mipmap-hdpi\ic_launcher.png" "android\app\src\main\res\mipmap-hdpi\ic_launcher_foreground.png"
-copy "android\app\src\main\res\mipmap-mdpi\ic_launcher.png" "android\app\src\main\res\mipmap-mdpi\ic_launcher_foreground.png"
-copy "android\app\src\main\res\mipmap-xhdpi\ic_launcher.png" "android\app\src\main\res\mipmap-xhdpi\ic_launcher_foreground.png"
-copy "android\app\src\main\res\mipmap-xxhdpi\ic_launcher.png" "android\app\src\main\res\mipmap-xxhdpi\ic_launcher_foreground.png"
-copy "android\app\src\main\res\mipmap-xxxhdpi\ic_launcher.png" "android\app\src\main\res\mipmap-xxxhdpi\ic_launcher_foreground.png"
-echo Foreground images created successfully!
-pause
